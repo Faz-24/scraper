618f70d3571685ebf9d6ca0943b99394b1191ab9
moshiroi <mqsas1337@gmail.com>
2024-02-24 02:34:49 +0000

    added poetry package manager, using relative paths for saving output file vs absolute path

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import os 
+import os
 import requests
 from bs4 import BeautifulSoup
 from docx import Document
-import time 
+import time
 
-Chap_No = list(range(1,5)) 
+Chap_No = list(range(1, 5))
 base_url = ["https://dulcet-scone-301d4e.netlify.app/chapter_"]
 doc = Document()
-counter = 0 
+counter = 0
 
 for i in Chap_No:
     counter += 1
@@ -15,23 +15,23 @@ for i in Chap_No:
     base_url.append(f"https://dulcet-scone-301d4e.netlify.app/chapter_{i}")
     response = requests.get(base_url[0])
 
-    #if response.status_code == 200:        
-    soup = BeautifulSoup(response.text, 'html.parser')
-    contents = soup.find('p')
-        
+    # if response.status_code == 200:
+    soup = BeautifulSoup(response.text, "html.parser")
+    contents = soup.find("p")
+
     if contents:
         chapter = contents.get_text(separator="\n \n")
-        doc.add_paragraph(chapter)    
-        
-    if counter == Chap_No: 
+        doc.add_paragraph(chapter)
+
+    if counter == Chap_No:
         break
     else:
         print(f"{counter}/{Chap_No[-1]}")
         print(f"{base_url}")
         base_url.pop()
-    #else:
-         #print("Failed to retrieve the webpage. Status code:", response.status_code)   
-destination_folder = "/Users/f.shahriyar/Documents"
-output_file = os.path.join(destination_folder, f"chapter-{counter}.docx")
+    # else:
+    # print("Failed to retrieve the webpage. Status code:", response.status_code)
+current_dir = os.path.dirname(__file__)
+output_file = os.path.join(current_dir, f"chapter-{counter}.docx")
 doc.save(output_file)
 print(f"Word document saved in chapter'{output_file}'")
